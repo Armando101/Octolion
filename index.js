bb8629b3d44353cb0d3705e05e7b012fd38982a4
Armando <rivera.armando997@gmail.com>
2020-08-25 12:54:31 -0500

    Octolion archivos estaticos

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 const productsRouter = require('./routes/products');
 
+// Colocamos la ruta static, cuando queramos acceder a algun recuso de public lo podemos hacer con /static
+app.use("/static", express.static(path.join(__dirname, "public")));
+
 // app.set('views', path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
@@ -10,4 +13,4 @@ app.use('/products', productsRouter);
 
 const server = app.listen(8000, function() {
     console.log(`Listening in http://localhost:${server.address().port}`);
-})
+});
